7a299f0ee215e277ef28ec19ecc33814674067e5
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-01 15:51:46 +0300

    router yapısı kuruldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Basket from "./pages/Basket";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
