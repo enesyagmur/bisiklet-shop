c41d827ff58ae8fd7e4d5b3df0825e4abc8bd905
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-01 15:56:50 +0300

    css dosyaları ve bağlantısı

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style/basket.css";
 
 const Basket = () => {
   return <div>Basket</div>;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style/home.css";
 
 const Home = () => {
   return <div>Home</div>;
