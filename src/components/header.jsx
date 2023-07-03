76e816daa665d41ca0341bfc016fede31a601c2f
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-03 11:45:39 +0300

    kabaca header için görünüm ayarlandı

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "../style/header.css";
+import { BsBicycle } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { AiOutlineHeart } from "react-icons/ai";
+import { SlBasket } from "react-icons/sl";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="profile"></div>
+
+      <div className="header-logo">
+        <h1>BIKE24</h1>
+      </div>
+
+      <div className="header-category">
+        <div>
+          <BsBicycle className="icon" />
+          <p>Bisikletler</p>
+        </div>
+        <div>
+          <BsBicycle className="icon" />
+          <p>Bisikletler</p>
+        </div>
+        <div>
+          <BsBicycle className="icon" />
+          <p>Bisikletler</p>
+        </div>
+
+        <div>
+          <AiOutlineMenu className="icon" />
+          <p>Hepsi</p>
+        </div>
+      </div>
+
+      <div className="header-search">
+        <input type="text" placeholder="ürün ara" />
+        <BsSearch className="icon" />
+      </div>
+
+      <div className="header-account">
+        <MdOutlineAccountCircle className="icon" />
+        <AiOutlineHeart className="icon" />
+        <SlBasket className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
