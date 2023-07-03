c2df39ba7c13d3cd0d018153792b142a8c1c51b9
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-03 11:45:49 +0300

    header da değişiklij

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../style/home.css";
+import Header from "../components/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
