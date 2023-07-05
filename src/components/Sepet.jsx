bb15a690f2574fc8542e8a0098374c3604aea492
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-05 11:33:36 +0300

    sepet gorunum değişikliği

--- src/components/Sepet.jsx
+++ src/components/Sepet.jsx
@@ -6,7 +6,20 @@ const Sepet = () => {
   const { componentState } = useSelector((state) => state.components);
   return (
     <div id="sepet" className={componentState[1] ? "sepetOpen" : "sepetClose"}>
-      Sepet
+      <p className="sepetTitle">Sepet</p>
+      <div className="sepetList">
+        <div className="sepetCount">
+          <p>Sepetiniz</p> <p>0</p>
+        </div>
+        <div className="sepetProducts">
+          <p>sepetiniz boş</p>
+        </div>
+        <div className="sepetPrice">
+          <p>Toplam tutar:</p> <p>TL</p>
+        </div>
+        <button className="orderBtn">Sipariş oluştur</button>
+        <button className="continueBtn">Alışverişe devam</button>
+      </div>
     </div>
   );
 };
