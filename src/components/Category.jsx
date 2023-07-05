438271f4d19fa262b85237095e7548c681da8589
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-05 11:08:12 +0300

    profile ve category de değişiklik

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,7 +6,15 @@ const Category = () => {
   const { componentState } = useSelector((state) => state.components);
   return (
     <div id="category" className={componentState[2] ? "open" : "close"}>
-      <p>Kasklar</p>
+      <p className="categoryTitle">Tüm kategoriler</p>
+      <div className="categoryList">
+        <p>Koruyucu ekipmanlar</p>
+        <p>Çantalar</p>
+        <p>Elektronik ekipmanlar</p>
+        <p>Takılabilir aksesuarlar</p>
+        <p>Tamir setleri ve ürünleri</p>
+        <p style={{ color: "orange" }}>İndirimde olan ürünler</p>
+      </div>
     </div>
   );
 };
