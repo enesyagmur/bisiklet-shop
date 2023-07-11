7f04e80d3ad53395bfe24fa4adb4b703d42209ff
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-07-11 12:58:26 +0300

    footerMain de değişiklik

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,11 @@ import "../style/components-style/footer.css";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { CiFacebook } from "react-icons/ci";
+import { FaCcMastercard } from "react-icons/fa";
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcPaypal } from "react-icons/fa";
+import { BsCashStack } from "react-icons/bs";
+
 import brands from "../data/brands.json";
 
 const Footer = () => {
@@ -18,10 +23,25 @@ const Footer = () => {
       </div>
       <div className="footerMain">
         <div className="footerHelp">
-          <p>Yardım</p>
+          <p>BIKE24 Hakkında</p>
+          <p>Şartlar ve koşullar</p>
+          <p>Gizlilik Politikası</p>
+          <p>Çerez Ayarları</p>
+          <p>Geri bildirim</p>
         </div>
-        <div className="footerPayMethods">
+        <div className="footerHelp">
+          <p>Yardum</p>
+          <p>Fiyat Garantisi</p>
+          <p>İadeler / Talepler</p>
+          <p>Nakliye ve montaj</p>
+          <p>İptal Hakkı</p>
+        </div>
+        <div className="footerPayMetods">
           <p>Ödeme</p>
+          <FaCcMastercard className="payMetodIcon" />
+          <FaCcVisa className="payMetodIcon" />
+          <FaCcPaypal className="payMetodIcon" />
+          <BsCashStack className="payMetodIcon" />
         </div>
       </div>
       <div className="footerBottom">
